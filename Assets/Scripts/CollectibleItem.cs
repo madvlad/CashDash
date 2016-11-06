2a1b4f8063474fe49679e1896529de41aa1418b1
David Bohan <dbohan@apterainc.com>
2016-11-06 14:21:08 -0500

    Initial commit

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleItem : MonoBehaviour {
+    public AudioClip CollectionSound;
+    public int ScoreBonus;
+    public int TimeBonusInSeconds;
+
+    private AudioSource source;
+    private bool AlreadyTriggered;
+	
+	void Start () {
+        source = gameObject.AddComponent<AudioSource>();
+        AlreadyTriggered = false;
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!AlreadyTriggered)
+        {
+            source.PlayOneShot(CollectionSound);
+            // TODO :: Add score and time to GameManager
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            Invoke("RemoveCollectible", 1.0f);
+        }
+    }
+
+    void RemoveCollectible()
+    {
+        Destroy(gameObject);
+    }
+}
