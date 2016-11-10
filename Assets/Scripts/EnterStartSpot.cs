851ac4e5d62ea6683ffa25f59a88dfeba46fff55
David Bohan <dbohan@apterainc.com>
2016-11-10 16:39:05 -0500

    Create start spot prefab

---
+++ Assets/Scripts/EnterStartSpot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterStartSpot : MonoBehaviour {
+
+    private GameManager gameManager;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        gameManager.SetIsAbleToStart(true);
+        if (gameManager.IsAbleToStart)
+        {
+            Debug.Log("Start");
+            gameManager.StartDash();
+        }
+    }
+}
